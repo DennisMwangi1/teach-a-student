e6dabf71db63e0b12a03382ce7290813d7288cc7
DennisMwangi1 <dennis.kamau@student.moringaschool.com>
2022-09-23 12:35:21 +0300

    craeted a react app and added landing page directory that will house the landing page components

---
+++ src/components/landing-page/navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <div>
+          <h2>Teach a student</h2> 
+          {/* <NavLink path='/' element={<h1>Home</h1>}>Home</NavLink> */}
+          
+    </div>
+  )
+}
+
+export default Navbar
