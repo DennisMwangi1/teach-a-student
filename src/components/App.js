bb3f941b1dee08db387900aee46925bde22c1fde
DennisMwangi1 <dennis.kamau@student.moringaschool.com>
2022-10-08 15:13:12 +0300

    modification for use of tailwind css

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
 import '../App.css';
-import Body from './landing-page/Body';
-import Footer from './landing-page/Footer';
-import Navbar from './landing-page/navbar/Navbar';
+// import Body from './landing-page/Body';
+// import Footer from './landing-page/Footer';
+// import Navbar from './landing-page/navbar/Navbar';
+import Student from './student_dashboard/student';
 
 function App() {
   return (
-    <div >
-      <Navbar />
-      <Body />
-      <Footer/>
+    // <div className='home bg-gradient-to-b from-yellow-200 via-pink-400 to-yellow-300' >
+    //   <Navbar className="fixed top-0 left-0 right-0"/>
+    //   <Body />
+    //   <Footer/>
+    // </div>
+    <div className='bg-slate-200 pt-10 pb-10'>
+      <Student/>
     </div>
   );
 }
