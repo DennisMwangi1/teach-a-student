4f288f7fe7fc5e3f41f2011ab30d64e574e2f209
DennisMwangi1 <dennis.kamau@student.moringaschool.com>
2022-10-08 15:11:48 +0300

    updated styling

--- src/components/landing-page/Body.js
+++ src/components/landing-page/Body.js
@@ -1,60 +1,121 @@
 import React from 'react'
-import student1 from './images/student1.jpg'
+import student1 from './images/student1.png'
+import books from './images/books.png'
+import book from './images/book.jpg'
+import DonutChart from 'react-donut-chart';
+import "animate.css/animate.min.css";
+import { AnimationOnScroll } from 'react-animation-on-scroll';
+import AnimatedText from 'react-animated-text-content';
 function Body() {
     return (
-        <>
-            <section className='section1'>
-                <div>
-                    <h1>An excellence center for education.</h1>
-                    <p>Teach a student is the online learning platform that offers student learning by students for students</p>
-                    <p>Have an enquiry?<br></br>Reach out to us via email below</p>
-                    <input type='email' placeholder='Enter your email...' />
-                    <input type='submit' value='Get Started' />
+        <main>
+            <section className='flex  '>
+                <div className=''>
+                    
+                    <h1 className='text-6xl font-sans font-black break-normal mt-40 ml-6 '>An <span className='before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 relative inline-block '>
+                        <span className="relative text-white">excellence</span></span> <br></br> center for education.</h1>
+                    <AnimatedText
+                        type="words" 
+                        animation={{
+                            x: '200px',
+                            y: '-20px',
+                            scale: 1.1,
+                            ease: 'ease-in-out',
+                        }}
+                        animationType="float"
+                        interval={0.06}
+                        duration={0.8}
+                        tag="p"
+                        className="animated-paragraph mt-6 ml-6 text-xl text-center tracking-wider italic"
+                        includeWhiteSpaces
+                        threshold={0.1}
+                        rootMargin="20%"
+                    >
+                        Teach a student  offers student learning by students for students.
+                    </AnimatedText>
+                    <p className='mt-6 ml-6 text-lg text-center tracking-wide '>Enter your E-mail below to get started<br></br>and receive a brochure of our services.</p>
+                    <form>
+                        <input className=' text-base p-[12px] ml-40 mt-6 rounded-full rounded-r-none  focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                    disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                    invalid:border-yellow-500 invalid:text-slate-600
+                    focus:invalid:border-yellow-500 focus:invalid:ring-yellow-500 ' type='email' placeholder='Enter your email...' />
+                        <input className='text-base p-[12px]  mt-6 rounded-full rounded-l-none bg-yellow-300 font-light hover:bg-yellow-400 ' type='submit' value='Get Started' />
+                    </form>
                 </div>
-                <div>
-                    <img src={student1} />
-                    <div>
-                        {/* icon here */}
-                        <p>Active students</p>
+                <div className='relative  p-60 '>
+                    <img className=' mt-10 border-dotted border-4 h-60 rounded-full absolute top-10 right-[16em] ' src={student1} alt='' />
+                    <img className=' mt-10 border-dotted border-4 h-60 top-60 left-[20.1em] rounded-full absolute ' src={student1} alt='' />
+                    <img className=' mt-10 border-dotted border-4 h-40  left-[12em] bottom-[5.2em] rounded-full absolute p-[1px] border-pink-400 ' src={book} alt='' />
+
+                    <div className='absolute left-80 top-[8em]'>
+                        <div className="mt-3 flex -space-x-2 ">
+                            <img className="inline-block h-12 w-12 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                            <img className="inline-block h-12 w-12 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                            <img className="inline-block h-12 w-12 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80" alt="" />
+                        </div>
+                        <p className='italic tracking-wider '> <span className='ml-8 text-xl font-black'>100+ </span> <br></br>Active Students</p>
                     </div>
-                    <div>
-                        {/* icon here */}
-                        <p>Members joined</p>
+                    <div className='absolute bottom-2 left-14 '>
+
+                        <div className='flex '>
+                            <div class="z-40 absolute bottom-14  bg-sky-500 p-3 rounded-full">05</div>
+                            <div className="z-30 absolute bottom-14 left-8 bg-red-500 p-3 rounded-full">04</div>
+                            <div className="z-20 absolute bottom-14 left-[65px] bg-blue-500 p-3 rounded-full">03</div>
+                        </div>
+
+                        <p className='italic tracking-wider'> <span className='ml-12 text-xl font-black'>1K+ </span> <br></br>Members joined</p>
                     </div>
                 </div>
             </section>
 
-            <section className='section2'>
-                <div>
-                    {/* icon here */}
-                    <p>Proffesional student teachers</p>
-                </div>
-                <div>
-                    {/*  */}
-                    <p>Students enrolled</p>
-                </div>
-            </section>
+            <AnimationOnScroll animateIn="animate__bounceIn" animateOnce={true}>
+                <section className='section2 flex justify-around mt-[120px]'>
 
-            <section className='section3'>
-                <h2>Why choose Teach A Student?</h2>
-                <p>We offer a wide range of tutoring options from teachers of the same age bracket as the student.<br></br>
+
+                    <div className='flex '>
+                        <img className='h-14  rounded-full bg-yellow-100 ' src={books} alt='' />
+                        <p className='mt-2 ml-8 text-xl'>50+ Professional and expert<br></br> student teachers</p>
+                    </div>
+
+                    <div className='flex '>
+                        <img className=' h-14  rounded-full bg-yellow-100 ' src={books} alt='' />
+                        <p className='mt-2 ml-6 text-xl'>100+ Students rate <br></br> and review</p>
+                    </div>
+
+                </section>
+            </AnimationOnScroll>
+
+            <section className='mt-20'>
+                <h2 className='text-center text-4xl font-bold tracking-wide'>Why choose Teach-A-Student?</h2>
+                <p className='text-center mt-4 text-base italic tracking-wider'>We offer a wide range of tutoring options from teachers of the same age bracket as the student.<br></br>
                     This creates a condusive and less pressured environment for students to learn and ask all the questions they have to the teacher.</p>
-                <div>
-                    <div>
-                        {/* icon here */}
-                        <p>Connect with our student teachers</p>
+                <div className='flex mt-20  justify-evenly'>
+                <AnimationOnScroll animateIn="animate__fadeInLeftBig" animateOnce={true}>
+                    <div className='flex-column  '>
+                        <img className=' h-28 ml-[110px] rounded-full bg-yellow-100 ' src={books} alt='' />
+                        <p className='mt-2  text-xl font-medium mb-2'>Connect with our student teachers</p>
+                        <small className=' text-center italic font-light text-sm '>Connect and interact with student teachers with ease.<br></br>Learn and understand fundamental concepts at you individual pace</small>
+                        </div>
+                    </AnimationOnScroll>
+                    
+                    <AnimationOnScroll animateIn="animate__jello" animateOnce={true}>
+                    <div className='flex-column justify-center  '>
+                        <img className=' h-28 ml-[110px] rounded-full bg-yellow-100 ' src={books} alt='' />
+                        <p className='mt-2 ml-6 text-xl'>Become a Student member</p>
+                        <small className=' text-center italic font-light text-sm '>Join our large number of students benefiting from this<br></br> program.Education is key</small>
                     </div>
-                    <div>
-                        {/* icon here */}
-                        <p>Become a student member</p>
+                    </AnimationOnScroll>
+
+                    <AnimationOnScroll animateIn="animate__fadeInRightBig" animateOnce={true}>
+                    <div className='flex-column justify-center '>
+                        <img className=' h-28 ml-[110px] rounded-full bg-yellow-100 ' src={books} alt='' />
+                        <p className='mt-2 ml-6 text-xl'>Become a Student teacher</p>
+                        <small className=' text-center italic font-light text-sm'>Join our group of student teachers empowering young minds<br></br> in the community around them</small>
                     </div>
-                    <div>
-                        {/* icon here */}
-                        <p>Become a teacher</p>
-                    </div>
+                    </AnimationOnScroll>
                 </div>
             </section>
-            <section>
+            <section className='mt-20'>
                 <div>
                     <h2>Analytics your daily,weekly and monthly activity</h2>
                     <p>We help track your progress as you have sessions with our student teachers</p>
@@ -64,7 +125,25 @@ function Body() {
                     <p>Track activity</p>
                 </div>
             </section>
-        </>
+            <DonutChart
+                data={[
+                    {
+                        label: 'Give you up',
+                        value: 25,
+                    },
+                    {
+                        label: 'jut',
+                        value: 25,
+                        //   isEmpty: true,
+                    },
+                    {
+                        label: 'mark',
+                        value: 50,
+                        //   isEmpty: true,
+                    },
+                ]}
+            />;
+        </main>
     )
 }
 
--- src/components/landing-page/navbar/Navbar.js
+++ src/components/landing-page/navbar/Navbar.js
@@ -1,16 +1,19 @@
 import React from 'react'
+import logo2 from '../images/logo2.png'
 // import { NavLink } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div className='navbar'>
-          <h2>Teach a student</h2> 
-          <a href='/'>Home</a>
-          <a href='/dashboard'>Parent/Student Dashboard</a>
-          <a href='/'>Teacher dashboard</a>
-      <button>Log in</button>
-      <button>Sign up</button>
-          
+    <div className='relative '>
+    <div className='navbar backdrop-opacity-50  bg-white/30 pb-12'>
+      
+          <span className='text-3xl font-black font-mono mt-6 ml-0 '> Teach-A-Student</span> 
+        <button className='bg-yellow-300  p-2 rounded-md font-serif transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-gradient-to-r from-sky-500 to-green-500 duration-300' >Log in as Student</button>
+        <button className='bg-yellow-300  p-2 rounded-md font-serif transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-gradient-to-r from-sky-500 to-green-500 duration-300' >Log in as Teacher</button>
+        <button className='bg-yellow-300  p-2 rounded-full font-serif transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-gradient-to-r from-pink-500 to-yellow-500 duration-300'>Sign up to be a Student</button>
+        <button className='bg-yellow-300  p-2 rounded-full font-serif transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-gradient-to-r from-pink-500 to-yellow-500 duration-300'>Sign up to be a Teacher</button>
+        
+      </div>
     </div>
   )
 }
