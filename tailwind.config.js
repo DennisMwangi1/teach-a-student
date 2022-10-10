226e0fb2db3b3d57d476d97b326ae07ea6177a40
DennisMwangi1 <dennis.kamau@student.moringaschool.com>
2022-10-10 13:22:42 +0300

    updated scripts and configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,14 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {},
+   theme: {
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+      'display': ['Oswald'],
+      'body': ['"Open Sans"'],
+    }
   },
   plugins: [],
 }
